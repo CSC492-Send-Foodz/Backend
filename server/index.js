566e8cd8ad82c9acb679b8dff3332e1907b83cb7
Fungucide <bluestich227@gmail.com>
2020-01-24 13:42:14 -0500

    SEN 17
    Added endpoint to index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const express = require('express');
+// const order = require('./Order');
+import Order from './Order.js'
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+var groceryStoreFunctions = express();
+
+// Adds a handler for POST requests to /groceryStore/placeOrder
+groceryStoreFunctions.post('/placeOrder', (request, response) => {
+    var body = request.body
+    newOrder = new order.Order(body);
+    console.log(newOrder);
+    response.send("POST received");
 });
+
+// Handles quesquests on /groceryStore
+exports.groceryStore = functions.https.onRequest(groceryStoreFunctions);
