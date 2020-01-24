0c0d6f82080be8fdb02e6134e1accb8c8f949b9f
Fungucide <bluestich227@gmail.com>
2020-01-24 14:45:47 -0500

    Typo from gs to fb

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const Order = require('./Order');
 
 // General request handler
-var groceryStoreFunctions = express();
+var foodBankFunctions = express();
 
-// Adds a handler for POST requests to '/groceryStore/placeOrder'
-groceryStoreFunctions.post('/placeOrder', (request, response) => {
+// Adds a handler for POST requests to '/foodBank/placeOrder'
+foodBankFunctions.post('/placeOrder', (request, response) => {
     var body = request.body;
     //Create a new Order object
     const order = new Order(body);
@@ -14,5 +14,5 @@ groceryStoreFunctions.post('/placeOrder', (request, response) => {
     response.send("POST received");
 });
 
-// Handles quesquests on '/groceryStore'
-exports.groceryStore = functions.https.onRequest(groceryStoreFunctions);
+// Handles quesquests on '/foodBank'
+exports.groceryStore = functions.https.onRequest(foodBankFunctions);
