38e2a526ff44a63a83464d22999f554981ba6471
parkkwa6 <kwangjin.park@mail.utoronto.ca>
2020-02-03 12:14:00 -0500

    Implement functionality for uploading inventory to firestore databse

--- server/index.js
+++ server/index.js
@@ -4,21 +4,23 @@ const express = require('express');
 const Order = require('./Order');
 const OrderProcessor = require('./OrderProcessor');
 const GroceryStoreService = require('./GroceryStoreService');
+const EdiOrder = require('./EdiOrder');
 
 var groceryStores = {};
 var processor = new OrderProcessor();
 var groceryStoreServ = new GroceryStoreService(groceryStores);
 var order;
 
+
 // Initialize App
 admin.initializeApp(functions.config().firebase);
 var gsDB = admin.firestore();
 
-
 /*******************Food Bank EndPoint *************************/
 
 // General request handler
-var foodBankFunctions = express();
+const foodBankFunctions = express();
+
 
 // Adds a handler for POST requests to '/foodBank/placeOrder'
 foodBankFunctions.post('/placeOrder', (request, response) => {
@@ -27,10 +29,11 @@ foodBankFunctions.post('/placeOrder', (request, response) => {
 
     //Create a new Order object
     order = new Order(body);
-
+    console.log("This is order:" + order);
     if (processor.processOrder(order, groceryStoreServ)) {
         response.status(200).send("Order Received");
     }
+    //write to firestore database
 });
 
 // Handles requests on '/foodBank'
@@ -40,45 +43,94 @@ exports.foodBank = functions.https.onRequest(foodBankFunctions);
 /*****************Grocery Store EndPoint **********************/
 var groceryStoreFunctions = express();
 
-//for parsing json data
-groceryStoreFunctions.use(express.json());
-
 //registers a grocery store
 groceryStoreFunctions.post('/sendUser', (request, response) => {
     
     var userInfo = request.body;
 
-    // TODO parse userInfo and register grocery store
+    //TODO parse userInfo and register grocery store
     groceryStores[userInfo.storeId] = userInfo;
     
     //status check works
     response.status(200).send("Grocery Store Registered");
 });
 
-//receive inventory update; receive entire GroceryStoreService inventory?
-groceryStoreFunctions.post('/inventoryUpdate', (request, response) =>{
-    //what would request look like?
-    //here we just need order Id to access orders in active order set
 
+//Update inventory of a store
+groceryStoreFunctions.post('/inventoryUpdate',(request, response) =>{
+        
+    //receive data body that is an inventory from single grocery store
+    var jsonBody = request.body; 
+    var newDict = new EdiOrder(jsonBody);
+
+    newInventoryToGroceryStoreData(newDict);
     
-    var orderid = request.body;
-    response.status(200).send("following is the OrderId: " + orderid.storeId);
-    
+    response.status(200).send("Inventory updated in Firestore");
 });
+
 
 //verify order has been picked up 
-groceryStoreFunctions.put('/orderPickedUp/:orderid', (request, response) =>{
-    //what would request look like?
-    //eg. 
-    var order2 = processor.getOrder(request.orderid);
-    console.log(order2);
-    console.log(processor.activeOrder);
-    response.status(200).send(order2.groceryId);
-    
+groceryStoreFunctions.post('/orderPickedUp/:orderId', (request, response) =>{
+
+    var tempOrder = processor.getOrder(request.orderId);
+
+    //New function here: query active order with specific id in the database
+    decrementInventoryFromGroceryStoreData();
 });
+
+exports.groceryStore = functions.https.onRequest(groceryStoreFunctions);
+
+/************************ Methods*************************************/
+
+//Function for updating inventory in firestore
+function newInventoryToGroceryStoreData(newDict){
+    //write to the database new inventory
+    var string_inventory_data = JSON.stringify(newDict.inventory);
+    var json_inventory = JSON.parse(string_inventory_data);
+    var batch = gsDB.batch();
+    for (var myKey in json_inventory){
+        var myKeyRef = gsDB.collection("GroceryStores").doc(`${newDict.groceryId}`).collection("InventoryCollection").doc("Items");
+        batch.set(myKeyRef, json_inventory);
+    }
+    batch.commit().then(function() {
+        console.log("Success!");
+        return null;
+    }).catch((err) => {
+        console.log('Error getting documents', err);
+        return err;
+    });
+}
+
+function decrementInventoryFromGroceryStoreData(order){
+    //order json object
+    gsDB.ref('groceryStore/').update({
+        order
+    });
+}
+
+function writeGroceryStoreData(storeId, companyName, location, storeNumber) {
+    gsDB.ref('groceryStore/' + storeId).set({
+    companyName: companyName,
+    location: location,
+    storeNumber: storeNumber
+    });
+}
+
+function updateGroceryStoreData(storeId, companyName, location, storeNumber) {
+    var update = {};
+    var updatedInfo = {
+        companyName: companyName,
+        location: location,
+        storeNumber: storeNumber
+    }
+    update['groceryStore/' + storeId] = updatedInfo;
+    gsDB.ref().update(update);
+}
 
 
 //Adding Data
+
+/*
 let gsDBRef = gsDB.collection('grocery').doc('store1');
 
 let setApple = gsDBRef.set({
@@ -97,7 +149,8 @@ let setPear = gsDBRef2.set({
     quantity: 9,
     expiryDate: "20 - 05- 30",
     groceryStoreId: 3
-}) 
+})
+*/ 
 
 /* Doesn't work
 db.collection('grocery').get()
@@ -114,41 +167,3 @@ db.collection('grocery').get()
 */
 
 // Handles requests on '/grocerystore'
-exports.groceryStore = functions.https.onRequest(groceryStoreFunctions)
-
-
-/************************ Methods*************************************/
-function newInventoryToGroceryStoreData(orderID){
-    console.log(orderID);
-    //gsDB.ref('groceryStore/' + storeId).set({
-    //    quantity: quantity
-    //});
-}
-
-function decrementInventoryFromGroceryStoreData(storeId, quantity){
-    gsDB.ref('groceryStore/' + storeId).update({
-        quantity: quantity
-    });
-}
-
-
-function writeGroceryStoreData(storeId, companyName, location, storeNumber) {
-    gsDB.ref('groceryStore/' + storeId).set({
-    companyName: companyName,
-    location: location,
-    storeNumber: storeNumber
-    });
-}
-
-function updateGroceryStoreData(storeId, companyName, location, storeNumber) {
-    var update = {};
-    var updatedInfo = {
-        companyName: companyName,
-        location: location,
-        storeNumber: storeNumber
-    }
-
-    update['groceryStore/' + storeId] = updatedInfo;
-
-    gsDB.ref().update(update);
-}
