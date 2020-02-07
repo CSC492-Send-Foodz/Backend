248f5ba20092449ca0a144a6aae9209e788c1ca0
Nada Elnour <nelnour90@gmail.com>
2020-02-07 11:56:02 -0500

    Enable setting and storing DAOs; processing order

--- server/OrderProcessor.js
+++ server/OrderProcessor.js
@@ -1,23 +1,44 @@
+const Driver = require('./Driver');
+
 class OrderProcessor {
-    constructor() {
-        this.activeOrder = {};
-        this.activeDriver = {};
-    }
+    constructor(driverQuery, activeOrderDao, groceryStoreDao, driverDao) {
+        this.initDriverListener(driverQuery);
+        this.activeOrderDao = activeOrderDao;
+        this.groceryStoreDao = groceryStoreDao;
+        this.driverDao = driverDao;
+    } 
 
-    getOrder(orderId){
-        //return order object
-        return this.activeOrder[orderId];
-    }
-
-    processOrder(order, gs) {
-        gs.updateStatus(order);
-
-        if (order.status !== 'Invalid') {
-            this.addOrderToDict(order);
-            console.log("Order created")
-            return true
+    processOrder(order) {
+        if (this.groceryStoreDao.isOrderValid(order)) {
+            let validDrivers = this.driverDao.findAllValidDrivers(order);
+            order.notifyDriver(validDrivers);
+            this.activeOrderDao.addToActiveOrders(order);
+        } else {
+            console.log("Order is invalid");
         }
-        return false;
+    }
+
+    initDriverListener(driverQuery) {
+        // Construct a listener for driver statuses
+        let observer = driverQuery.onSnapshot(querySnapshot => {
+            querySnapshot.docChanges().forEach(change => {
+                // Get the driver data
+                var driver = change.doc.data();
+                driver.driverId = change.doc.ref.id;
+                var driverObj = Driver(driver);
+                // Update the orders if the driver is Available
+                if (driver.status === 'Available') {
+                    // Find orders that the driver can deliver and send
+                    notifyDriver(driverObj, this.activeOrderDao.findMatchingActiveOrders(driverObj));
+                }
+            });
+        });
+    }
+
+    notifyDriver(driver, orders) {
+        orders.forEach(order => {
+            order.notifyDriver([driver])
+        });
     }
 
     addOrderToDict(order) {
