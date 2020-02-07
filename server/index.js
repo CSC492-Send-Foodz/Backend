23be29f06a5a7858b6bb640462fba2630f711308
Nada Elnour <nelnour90@gmail.com>
2020-02-06 22:40:32 -0500

    Pass activeOrdersDao to OrderProcessor and generate a unique key

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ const OrderProcessor = require('./OrderProcessor');
 const GroceryStoreService = require('./GroceryStoreService');
 const EdiOrder = require('./EdiOrder');
 const GroceryStoreDao = require('./GroceryStoreDao');
+const activeOrdersDao = require('./ActiveOrdersDao');
 
 // Initialize App
 admin.initializeApp(functions.config().firebase);
 var gsDB = admin.firestore();
 var groceryStores = {};
-var processor = new OrderProcessor();
-var  groceryStoreService = new GroceryStoreService(groceryStores);
+var processor = new OrderProcessor(activeOrdersDao);
+var groceryStoreService = new GroceryStoreService(groceryStores);
 var groceryStoreDao = new GroceryStoreDao(gsDB)
 
 /*******************Food Bank EndPoint *************************/
@@ -22,8 +23,12 @@ const foodBankFunctions = express();
 
 foodBankFunctions.post('/placeOrder', (request, response) => {
     var body = request.body;
+    let orderId = activeOrdersDao.generateUniqueKey();
     order = new Order(body);
-    if (processor.processOrder(order,  groceryStoreService)) {
+    
+    order.setOrderId(orderId);
+
+    if (processor.processOrder(order, groceryStoreService)) {
         response.status(200).send("Order Received");
     }
     //write to firestore database
@@ -36,15 +41,15 @@ var groceryStoreFunctions = express();
 groceryStoreFunctions.post('/sendUser', (request, response) => {
     var userInfo = request.body;
     //TODO parse userInfo and register grocery store
-    groceryStores[userInfo.storeId] = userInfo; 
+    groceryStores[userInfo.storeId] = userInfo;
     response.status(200).send("Grocery Store Registered");
 });
 
 
 //Update inventory of a store
-groceryStoreFunctions.post('/inventoryUpdate',(request, response) =>{
+groceryStoreFunctions.post('/inventoryUpdate', (request, response) => {
     //receive data body that is an inventory from single grocery store
-    var jsonBody = request.body; 
+    var jsonBody = request.body;
     var newDict = new EdiOrder(jsonBody);
     groceryStoreDao.newInventoryToGroceryStoreData(newDict);
     response.status(200).send("Inventory updated in Firestore");
@@ -52,7 +57,7 @@ groceryStoreFunctions.post('/inventoryUpdate',(request, response) =>{
 
 
 //verify order has been picked up 
-groceryStoreFunctions.post('/orderPickedUp/:orderId', (request, response) =>{
+groceryStoreFunctions.post('/orderPickedUp/:orderId', (request, response) => {
     var tempOrder = processor.getOrder(request.orderId);
     //New function here: query active order with specific id in the database
     groceryStoreDao.decrementInventoryFromGroceryStoreData();
