38e5a1b2c3ba9df18ffe56e5fa4c74e255239440
khushiDesai <59741309+khushiDesai@users.noreply.github.com>
2020-02-15 14:21:00 -0500

    Update and rename FoodBankModel.js to FoodBank.js

--- server/FoodBank.js
+++ server/FoodBank.js
@@ -1,5 +1,5 @@
-class FoodBankModel{
-    contructor(foodbankRef){
+class FoodBank{
+    constructor(foodbankRef){
         this.foodBankID= foodbankRef.foodBankID,
         this.fbName = foodbankRef.fbName,
         this.fblocation=foodbankRef.fblocation,
@@ -20,5 +20,7 @@ class FoodBankModel{
     setFoobBankLocationId(fblocationNumber){this.fblocationNumber=fblocationNumber;}
     
     getFoodBankLocationId(){ return this.fblocationNumber; }
+
 }
 
+module.exports = FoodBank;
